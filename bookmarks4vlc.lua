7aca4684748c0a7524e7231aa272fb25b2887ac5
Paul Wicks <pwicks86@gmail.com>
2012-02-22 19:58:47 -0800

    added gpl header

--- bookmarks4vlc.lua
+++ bookmarks4vlc.lua
@@ -1,3 +1,20 @@
+-- bookmarks4vlc.lua - A simple bookmark system for vlc
+--Copyright (C) 2012 - Paul Wicks
+
+--This program is free software; you can redistribute it and/or
+--modify it under the terms of the GNU General Public License
+--as published by the Free Software Foundation; either version 2
+--of the License, or (at your option) any later version.
+
+--This program is distributed in the hope that it will be useful,
+--but WITHOUT ANY WARRANTY; without even the implied warranty of
+--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--GNU General Public License for more details.
+
+--You should have received a copy of the GNU General Public License
+--along with this program; if not, write to the Free Software
+--Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-----------------------------------------------------------------------------------
  --[[
 Bookmarks for vlc that actually work! 
  ]]
